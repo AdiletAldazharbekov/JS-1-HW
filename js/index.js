6edc16ec017c20af610f6e370abf8e831b54c66b
Adilet Aldazharbekov <aldajarbekov@gmail.com>
2022-02-22 18:48:24 +0600

    pre

--- js/index.js
+++ js/index.js
@@ -1,27 +1,22 @@
 let num = 3
-console.log(alert(num))
 
 let a=10
 let b=2
-console.log(alert(a+b))
-console.log(alert(a-b))
-console.log(alert(a*b))
-console.log(alert(a/b))
+
 
 let c=15
 let d=2
 let result = c+d
-console.log(alert(result))
+
 
 let str='Привет Мир!'
-console.log(alert(str))
+
 
 let str1='Привет, '
 let str2='Мир!'
-console.log(alert(str1+str2))
+
 
 let name ='Adilet'
-console.log(alert(str1+name))
 
 // let name=document.getElementById("name")
 // let lastName=document.getElementById("lastName")
@@ -39,4 +34,19 @@ const student = {
 
 function register() {
    alert("Регистрация прошла!")
+   console.log(alert(num))
+
+   console.log(alert(a+b))
+console.log(alert(a-b))
+console.log(alert(a*b))
+console.log(alert(a/b))
+
+console.log(alert(result))
+
+console.log(alert(str))
+
+console.log(alert(str1+str2))
+
+console.log(alert(str1+name))
+
 } 
