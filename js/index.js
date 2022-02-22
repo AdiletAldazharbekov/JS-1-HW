2321d094f067c7c34471f95f85544a6d0e308246
Adilet Aldazharbekov <aldajarbekov@gmail.com>
2022-02-22 13:39:26 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+let num = 3
+
+// console.log(alert(num))
+
+let a=10
+let b=2
+// console.log(alert(a+b))
+// console.log(alert(a-b))
+// console.log(alert(a*b))
+// console.log(alert(a/b))
+
+let c=15
+let d=2
+let result = c+d
+// console.log(alert(result))
+
+let str='Привет Мир!'
+// console.log(alert(str))
+
+let str1='Hello, '
+let str2='World!'
+
+// console.log(alert(str1+str2))
+
+let name ='Adilet'
+let lastName='Aldazharbekov'
+let age=37
+let course='JavaScript'
+let durationMonth=3
+
+// console.log(alert(str1+name))
+
+const student {
+    name: name,
+    lastName: lastName,
+    age: age,
+    course: course,
+    durationMonth: durationMonth
+}
