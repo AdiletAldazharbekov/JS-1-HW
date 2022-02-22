6a281f0b8560978500c5e64a426a0c9a46cede08
Adilet Aldazharbekov <aldajarbekov@gmail.com>
2022-02-22 13:44:36 +0600

    js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
 let num = 3
 
-// console.log(alert(num))
+console.log(alert(num))
 
 let a=10
 let b=2
-// console.log(alert(a+b))
-// console.log(alert(a-b))
-// console.log(alert(a*b))
-// console.log(alert(a/b))
+console.log(alert(a+b))
+console.log(alert(a-b))
+console.log(alert(a*b))
+console.log(alert(a/b))
 
 let c=15
 let d=2
 let result = c+d
-// console.log(alert(result))
+console.log(alert(result))
 
 let str='Привет Мир!'
-// console.log(alert(str))
+console.log(alert(str))
 
 let str1='Hello, '
 let str2='World!'
 
-// console.log(alert(str1+str2))
+console.log(alert(str1+str2))
 
 let name ='Adilet'
 let lastName='Aldazharbekov'
@@ -28,7 +28,7 @@ let age=37
 let course='JavaScript'
 let durationMonth=3
 
-// console.log(alert(str1+name))
+console.log(alert(str1+name))
 
 const student {
     name: name,
@@ -37,3 +37,5 @@ const student {
     course: course,
     durationMonth: durationMonth
 }
+
+console.log(student)
