bb61dedbf5d6b0eed34caf98c6f403adeac14f1d
Adilet Aldazharbekov <aldajarbekov@gmail.com>
2022-02-22 18:07:26 +0600

    bootstrap-v4

--- js/index.js
+++ js/index.js
@@ -1,41 +1,42 @@
-// let num = 3
-// console.log(alert(num))
+let num = 3
+console.log(alert(num))
 
-// let a=10
-// let b=2
-// console.log(alert(a+b))
-// console.log(alert(a-b))
-// console.log(alert(a*b))
-// console.log(alert(a/b))
+let a=10
+let b=2
+console.log(alert(a+b))
+console.log(alert(a-b))
+console.log(alert(a*b))
+console.log(alert(a/b))
 
-// let c=15
-// let d=2
-// let result = c+d
-// console.log(alert(result))
+let c=15
+let d=2
+let result = c+d
+console.log(alert(result))
 
-// let str='Привет Мир!'
-// console.log(alert(str))
+let str='Привет Мир!'
+console.log(alert(str))
 
-// let str1='Привет, '
-// let str2='Мир!'
-// console.log(alert(str1+str2))
+let str1='Привет, '
+let str2='Мир!'
+console.log(alert(str1+str2))
 
-// let name ='Adilet'
-// console.log(alert(str1+name))
+let name ='Adilet'
+console.log(alert(str1+name))
 
+let name=document.getElementById("name")
+let lastName=document.getElementById("lastName")
+let age=document.getElementById("age").value;
+let durationMonth=document.getElementById("durationMonth")
+let course=document.getElementById("course")
 
-// let lastName='Aldazharbekov'
-// let age=37
-// let course='JavaScript'
-// let durationMonth=3
+const student = {
+    name: name,
+    lastName: lastName,
+    age: age,
+    durationMonth: durationMonth,
+    course: course,
+};
 
-
-// const student = {
-//     name: name,
-//     lastName: lastName,
-//     age: age,
-//     course: course,
-//     durationMonth: durationMonth,
-// };
-
-// console.log(student)
+function register() {
+   alert("Регистрация прошла!")
+} 
