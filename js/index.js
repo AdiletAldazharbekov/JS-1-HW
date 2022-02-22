bec81b7d34a7a4f86a48c44d1eb15f9f9d67ca37
Adilet Aldazharbekov <aldajarbekov@gmail.com>
2022-02-22 17:03:16 +0600

    bootstrap

--- js/index.js
+++ js/index.js
@@ -1,41 +1,41 @@
-let num = 3
+// let num = 3
 
-console.log(alert(num))
+// console.log(alert(num))
 
-let a=10
-let b=2
-console.log(alert(a+b))
-console.log(alert(a-b))
-console.log(alert(a*b))
-console.log(alert(a/b))
+// let a=10
+// let b=2
+// console.log(alert(a+b))
+// console.log(alert(a-b))
+// console.log(alert(a*b))
+// console.log(alert(a/b))
 
-let c=15
-let d=2
-let result = c+d
-console.log(alert(result))
+// let c=15
+// let d=2
+// let result = c+d
+// console.log(alert(result))
 
-let str='Привет Мир!'
-console.log(alert(str))
+// let str='Привет Мир!'
+// console.log(alert(str))
 
-let str1='Hello, '
-let str2='World!'
+// let str1='Hello, '
+// let str2='World!'
 
-console.log(alert(str1+str2))
+// console.log(alert(str1+str2))
 
-let name ='Adilet'
-let lastName='Aldazharbekov'
-let age=37
-let course='JavaScript'
-let durationMonth=3
+// let name ='Adilet'
+// let lastName='Aldazharbekov'
+// let age=37
+// let course='JavaScript'
+// let durationMonth=3
 
-console.log(alert(str1+name))
+// console.log(alert(str1+name))
 
-const student = {
-    name: name,
-    lastName: lastName,
-    age: age,
-    course: course,
-    durationMonth: durationMonth,
-};
+// const student = {
+//     name: name,
+//     lastName: lastName,
+//     age: age,
+//     course: course,
+//     durationMonth: durationMonth,
+// };
 
-console.log(student)
+// console.log(student)
